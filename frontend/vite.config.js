22793ad9edd05a94dc248e4455b08d92cf6191de
Prakhar Sinha <prakharsinha3125@gmail.com>
2026-03-05 15:27:13 +0530

    fix22

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Create a shortcut called /api-sharepoint
+      '/api-sharepoint': {
+        target: 'https://iontradingcom.sharepoint.com',
+        changeOrigin: true,
+        secure: false,
+        // This removes the "/api-sharepoint" prefix before sending the request to MS
+        rewrite: (path) => path.replace(/^\/api-sharepoint/, '')
+      }
+    }
+  }
 })
